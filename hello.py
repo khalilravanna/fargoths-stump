3279a0c16077c983d212a3767d3cec5f582bce86
Stefan Valentin <khalilravanna@gmail.com>
2013-07-25 10:57:22 -0400

    json-ed output and added timestamp

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, url_for
-import urllib2
+from flask import Flask, render_template, url_for, request
+import urllib2, json
 from BeautifulSoup import BeautifulSoup
 
 app = Flask(__name__)
@@ -8,20 +8,24 @@ app = Flask(__name__)
 with app.test_request_context():
 	url_for('static', filename='fourohfargoth.png')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
-	import urllib2
-	from BeautifulSoup import BeautifulSoup
+	import datetime, time
 
-	soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
-	try: 
-		image = soup.find(attrs={"class": "image"}).img['src']
-	except:
-		image = 'static/fourohfargoth.png'
+	if request.method == 'GET':
 
-	return (render_template('scroll.html', image=image), 200, {'Access-Control-Allow-Origin': '*'})
-	#print resp
-	#resp.headers['Access-Control-Allow-Origin'] = '*'
-	#return resp
+		now = datetime.datetime.now()
+		now_readable = "%d/%d/%d %d:%d" % (now.month, now.day, now.year, now.hour, now.minute)
+
+		soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
+		try: 
+			image = soup.find(attrs={"class": "image"}).img['src']
+		except:
+			image = 'static/fourohfargoth.png'
+
+		if request.args.get('json'):
+			return json.dumps({'image': image, 'request_time': now_readable})
+		else:
+			return (render_template('scroll.html', image=image), 200, {'Access-Control-Allow-Origin': '*'})
 
 #app.run()
