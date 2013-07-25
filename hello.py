372e168f73360f3a1aa5b8356e6891a0eca0089a
Stefan Valentin <khalilravanna@gmail.com>
2013-07-25 00:22:44 -0400

    added image grabber at hello

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+import urllib2
+from BeautifulSoup import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+	soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
+	return soup.img['src']
