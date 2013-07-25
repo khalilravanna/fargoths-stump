6ea51f168843e3eb1ab730f16cc4829660cd3c8b
Stefan Valentin <khalilravanna@gmail.com>
2013-07-25 00:39:52 -0400

    moved image to static folder

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
 app = Flask(__name__)
+
+url_for('static', filename='fourohfargoth.png')
 
 @app.route('/')
 def hello():
