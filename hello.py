90e21508182c4640f8fc2c34b34d4947167398a4
Stefan Valentin <khalilravanna@gmail.com>
2013-07-25 00:30:49 -0400

    forgot import

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
@@ -8,4 +8,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
-	return soup.img['src']
+	image = soup.img['src']
+	return render_template('scroll.html', image=image)
