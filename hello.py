300efbe6a9d55c41b8f547d5f9edead7c7009fff
Stefan Valentin <khalilravanna@gmail.com>
2013-07-25 11:08:37 -0400

    added fix for json return of 404

--- hello.py
+++ hello.py
@@ -20,10 +20,14 @@ def hello():
 		soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
 		try: 
 			image = soup.find(attrs={"class": "image"}).img['src']
+			success = True
 		except:
-			image = 'static/fourohfargoth.png'
+			image = "static/fourohfargoth.png"
+			success = False
 
 		if request.args.get('json'):
+			if not success:
+				image = "http://fargoths-stump.herokuapp.com/" + image
 			return (json.dumps({'image': image, 'request_time': now_readable}), 200, {'Access-Control-Allow-Origin': '*'})
 		else:
 			return (render_template('scroll.html', image=image), 200, {'Access-Control-Allow-Origin': '*'})
