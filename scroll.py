f1ddaab3ed7f48dd7ed6ddbd99773ca119f0970b
Stefan Valentin <khalilravanna@gmail.com>
2013-08-04 14:15:52 -0400

    added first draft of pulling npc data from uesp

--- scroll.py
+++ scroll.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+@app.route("/favicon.ico")
+def favicon():
+    return app.send_static_file("favicon.ico")
+
 @app.route('/', methods=['GET'])
 def hello():
 	import datetime, time
@@ -33,19 +37,60 @@ def hello():
 
 @app.route('/foundation', methods=['GET'])
 def foundation():
-	if request.method == 'GET':
-		soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
-		try: 
-			image = soup.find(attrs={"class": "image"}).img['src']
-			success = True
-		except:
-			image = "static/images/fourohfargoth.png"
-			success = False
+	soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
+	try: 
+		image = soup.find(attrs={"class": "image"}).img['src']
+		success = True
+	except:
+		image = "static/images/fourohfargoth.png"
+		success = False
 
-		content = soup.find('p')
+	content = soup.find('p')
 
-		print content
+	print content
 
-		return render_template('scroll-foundation.html', image=image, content=content)
+	return render_template('scroll-foundation.html', image=image, content=content)
 
-#app.run()
+@app.route('/test', methods=['GET'])
+def test():
+	import random
+	from bs4 import BeautifulSoup
+
+	# hardcode urls for npc pages
+	npc_urls = ['http://www.uesp.net/wiki/Category:Morrowind-NPC_Images', 
+		'http://www.uesp.net/w/index.php?title=Category:Morrowind-NPC_Images&filefrom=Chirranirr%0AMW-npc-Chirranirr.jpg#mw-category-media',
+		'http://www.uesp.net/w/index.php?title=Category:Morrowind-NPC_Images&filefrom=Felayn+Andral%0AMW-npc-Felayn+Andral.jpg#mw-category-media',
+		'http://www.uesp.net/w/index.php?title=Category:Morrowind-NPC_Images&filefrom=Janand+Maulinie%0AMW-npc-Janand+Maulinie.jpg#mw-category-media',
+		'http://www.uesp.net/w/index.php?title=Category:Morrowind-NPC_Images&filefrom=Nelso+Salenim%0AMW-npc-Nelso+Salenim.jpg#mw-category-media',
+		'http://www.uesp.net/w/index.php?title=Category:Morrowind-NPC_Images&filefrom=Skeetul%0AMW-npc-Skeetul.jpg#mw-category-media',
+		'http://www.uesp.net/w/index.php?title=Category:Morrowind-NPC_Images&filefrom=Yambagorn+gor-Shulor%0AMW-npc-Yambagorn+gor-Shulor.jpg#mw-category-media']
+
+	random_page = random.randint(0, len(npc_urls)-1)
+	soup = BeautifulSoup(urllib2.urlopen(npc_urls[random_page]))
+	items = soup.find_all('li', 'gallerybox')
+
+	num = random.randint(0, len(items)-1)
+
+	link = items[num].find('a').get('href')
+	link = 'http://www.uesp.net' + link
+
+	soup = BeautifulSoup(urllib2.urlopen(link))
+	image = soup.find('div', 'fullImageLink').find('a').get('href')
+
+	page = soup.find(id='mw-imagepage-linkstoimage-ns110').find('a').get('href') # ns110 seems to be the code for the actual page
+	page = 'http://www.uesp.net' + page
+	
+	soup = BeautifulSoup(urllib2.urlopen(page))
+	paragraphs = soup.find('div', 'mw-content-ltr').find_all('p') 
+	title = paragraphs[6].find('b').text
+	content = ''
+	# for some reason the 7th paragrph is the start of the ones about the character
+	for num in range(6, len(paragraphs)):
+		content = content + str(paragraphs[num])
+	print content
+
+	return render_template('scroll-foundation.html', image=image, content=content, title=title)
+
+
+
+app.run()
