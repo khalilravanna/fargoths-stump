65e8d0adf673c508028c1c8d89a28e097acfe843
Stefan Valentin <khalilravanna@gmail.com>
2013-11-24 01:33:49 -0600

    Hooked up mongodb. Added basic page_view count

--- scroll.py
+++ scroll.py
@@ -1,18 +1,42 @@
 import os
 from flask import Flask, render_template, url_for, request
+from urlparse import urlparse
 import urllib2, json
 from bs4 import BeautifulSoup
+from pymongo import Connection
 
 app = Flask(__name__)
 
 debug = False
+mock_data = False
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+ 
+if MONGO_URL:
+  # Get a connection
+  connection = Connection(MONGO_URL)
+  # Get the database
+  db = connection[urlparse(MONGO_URL).path[1:]]
+else:
+  # Not on an app with the MongoHQ add-on, do some localhost action
+  connection = Connection('localhost', 27017)
+  db = connection['MyDB']
 
 @app.route("/favicon.ico")
 def favicon():
     return app.send_static_file("favicon.ico")
 
+def increment_page_views():
+	myObj = db.analytics.find_one({'event':'page_views'})
+	if not myObj:
+  		myObj = {'event':'page_views', 'count':1}
+	else:
+		myObj['count'] += 1
+	db.analytics.save(myObj)
+
 @app.route('/', methods=['GET'])
 def home():
+	increment_page_views()
 	return render_template('scroll-angular.html')
 
 @app.route('/old', methods=['GET'])
@@ -69,7 +93,7 @@ def npc():
 	import json, random, time
 	from bs4 import BeautifulSoup
 
-	if debug:
+	if mock_data:
 		return json.dumps({"content": "<p><b>Skink-in-Tree's-Shade</b>, an <a href=\"http://www.uesp.net/wiki/Morrowind:Argonian\" title=\"Morrowind:Argonian\">Argonian</a> <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Sorcerer\" title=\"Morrowind:Sorcerer\">sorcerer</a>, resides in the <a href=\"http://www.uesp.net/wiki/Morrowind:Mage%27s_Guild_(Wolverine_Hall)\" title=\"Morrowind:Mage's Guild (Wolverine Hall)\">Mage's Guild</a> at Wolverine Hall near <a href=\"http://www.uesp.net/wiki/Morrowind:Sadrith_Mora\" title=\"Morrowind:Sadrith Mora\">Sadrith Mora</a>. He is the primary quest giver there and can allow you to join the guild.</p><p>He wears a <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Common_Shirt\" title=\"Morrowind:Common Shirt\">common shirt</a> with matching <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Common_Pants\" title=\"Morrowind:Common Pants\">pants</a>, and he wields a <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Glass_Dagger\" title=\"Morrowind:Glass Dagger\">glass dagger</a>. Like all Argonians, he is <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Resist_Poison\" title=\"Morrowind:Resist Poison\">immune to poison</a>, has a <a href=\"http://www.uesp.net/wiki/Morrowind:Resist_Common_Disease\" title=\"Morrowind:Resist Common Disease\">resistance to disease</a>, and can <a href=\"http://www.uesp.net/wiki/Morrowind:Water_Breathing\" title=\"Morrowind:Water Breathing\">breathe underwater</a>. Aside from that, he knows: <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Burden_of_Sin\" title=\"Morrowind:Spells/Burden of Sin\">Burden of Sin</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Crushing_Burden\" title=\"Morrowind:Spells/Crushing Burden\">Crushing Burden</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Crushing_Burden_of_Sin\" title=\"Morrowind:Spells/Crushing Burden of Sin\">Crushing Burden of Sin</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Cruel_Weary\" title=\"Morrowind:Spells/Cruel Weary\">Cruel Weary</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Great_Feather\" title=\"Morrowind:Spells/Great Feather\">Great Feather</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Summon_Greater_Bonewalker\" title=\"Morrowind:Spells/Summon Greater Bonewalker\">Summon Greater Bonewalker</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Summon_Scamp\" title=\"Morrowind:Spells/Summon Scamp\">Summon Scamp</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Wizard_Rend\" title=\"Morrowind:Spells/Wizard Rend\">Wizard Rend</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Commanding_Touch\" title=\"Morrowind:Spells/Commanding Touch\">Commanding Touch</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Reflect\" title=\"Morrowind:Spells/Reflect\">Reflect</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Weak_Spelldrinker\" title=\"Morrowind:Spells/Weak Spelldrinker\">Weak Spelldrinker</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Wild_Spelldrinker\" title=\"Morrowind:Spells/Wild Spelldrinker\">Wild Spelldrinker</a>, <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Slowfall\" title=\"Morrowind:Spells/Slowfall\">Slowfall</a>, and <a class=\"mw-redirect\" href=\"http://www.uesp.net/wiki/Morrowind:Spells/Summon_Skeletal_Minion\" title=\"Morrowind:Spells/Summon Skeletal Minion\">Summon Skeletal Minion</a>.</p><p>This humble Argonian holds his difficult Mages Guild branch with grace and diplomacy. Of all Guild leaders he also seems the most concerned about Vvardenfell's contemporary events. Dark afflictions such as vampirism and ash creatures appear as his chosen field of academic study. Despite being a Mages Guild member, he is somewhat respected by <a href=\"http://www.uesp.net/wiki/Morrowind:House_Telvanni\" title=\"Morrowind:House Telvanni\">House Telvanni</a>, having been noted by them as being <i>\"almost as sage and learned as Telvanni aspirants five times his age\"</i>. He is also rumored to carry his anti-<a href=\"http://www.uesp.net/wiki/Morrowind:Slavery\" title=\"Morrowind:Slavery\">slavery</a> politics a little too far, much farther than Imperial law would normally allow.</p><p>Skink-in-Tree's-Shade is the <a href=\"http://www.uesp.net/wiki/Morrowind:Master_Trainers\" title=\"Morrowind:Master Trainers\">Master Trainer</a> for <a href=\"http://www.uesp.net/wiki/Morrowind:Speechcraft\" title=\"Morrowind:Speechcraft\">Speechcraft</a> and also provides medium <a href=\"http://www.uesp.net/wiki/Morrowind:Trainers\" title=\"Morrowind:Trainers\">training</a> in <a href=\"http://www.uesp.net/wiki/Morrowind:Mysticism\" title=\"Morrowind:Mysticism\">Mysticism</a> and <a href=\"http://www.uesp.net/wiki/Morrowind:Enchant\" title=\"Morrowind:Enchant\">Enchant</a>, and is the second-highest enchant after the hostile master trainer, <a href=\"http://www.uesp.net/wiki/Morrowind:Qorwynn\" title=\"Morrowind:Qorwynn\">Qorwynn</a>. However, you must be at least a <a href=\"http://www.uesp.net/wiki/Morrowind:Mages_Guild#Mages_Guild_Ranks\" title=\"Morrowind:Mages Guild\">Magician</a> in the Mages Guild to receive Skink's services.</p>", "image": "http://images.uesp.net/thumb/1/13/MW-npc-Skink-in-Tree%27s-Shade.jpg/600px-MW-npc-Skink-in-Tree%27s-Shade.jpg", "title": "Skink-in-Tree's-Shade"})
 	start = time.time();
 
@@ -142,5 +166,6 @@ def npc():
 
 	#return render_template('scroll-angular.html', image=image, content=content, title=title)
 	return (json.dumps({'title': title, 'image': image, 'content': content}), 200, {'Access-Control-Allow-Origin': '*'})
+
 if debug:
 	app.run(debug=debug)
