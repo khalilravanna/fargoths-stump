f4741f85283b44237e9366bd830a2c50d3bb9e15
Stefan Valentin <khalilravanna@gmail.com>
2014-12-11 17:50:56 -0600

    Cleaned up some old routes that weren't being used

--- scroll.py
+++ scroll.py
@@ -43,46 +43,6 @@ def home():
 	increment_page_views()
 	return render_template('scroll-angular.html')
 
-@app.route('/old', methods=['GET'])
-def oldStump():
-	import datetime, time
-
-	if request.method == 'GET':
-
-		now = datetime.datetime.now()
-		now_readable = "%d/%d/%d %d:%d" % (now.month, now.day, now.year, now.hour, now.minute)
-
-		soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
-		try:
-			image = soup.find(attrs={"class": "image"}).img['src']
-			success = True
-		except:
-			image = "static/images/fourohfargoth.png"
-			success = False
-
-		content = soup.find('p')
-
-		if request.args.get('json'):
-			if not success:
-				image = "http://fargoths-stump.herokuapp.com/" + image
-			return (json.dumps({'image': image, 'request_time': now_readable}), 200, {'Access-Control-Allow-Origin': '*'})
-		else:
-			return (render_template('scroll.html', image=image), 200, {'Access-Control-Allow-Origin': '*'})
-
-@app.route('/foundation', methods=['GET'])
-def foundation():
-	soup = BeautifulSoup(urllib2.urlopen('http://uesp.net/wiki/Special:Random'))
-	try:
-		image = soup.find(attrs={"class": "image"}).img['src']
-		success = True
-	except:
-		image = "static/images/fourohfargoth.png"
-		success = False
-
-	content = soup.find('p')
-
-	return render_template('scroll-foundation.html', image=image, content=content)
-
 def get_full_image(link, lores=False):
 	parts = link.split('/')
 	parts.pop(3)
